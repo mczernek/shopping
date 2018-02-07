020beb917db52de4ae84279ef25a102ea58bd2c4
Michał Czernek <michal.czernek@dreamlab.pl>
2018-02-07 22:14:07 +0100

    Correct package and add android architecture components.

--- app/src/main/java/pl/mczernek/shoppinglists/PredefinedListsAcitivty.kt
+++ app/src/main/java/pl/mczernek/shoppinglists/PredefinedListsAcitivty.kt
@@ -1,4 +1,4 @@
-package shoppinglists.mczernek.pl.shoppinglists
+package pl.mczernek.shoppinglists
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/pl/mczernek/shoppinglists/data/ListDefinition.kt
@@ -0,0 +1,2 @@
+package pl.mczernek.shoppinglists.data
+
